1d7103c2d4ce3b87bc232beb5d1fddbd4a892ca8
Luanpda <luanpope11@gmail.com>
2025-04-06 22:00:15 -0300

    .

---
+++ pagina.js
@@ -0,0 +1,31 @@
+const frenteFC = document.getElementById("titulo")
+const trasFC = document.getElementById("texto")
+const Criar = document.getElementById("criar")
+const containerFC = document.getElementById("containerFC")
+
+Criar.addEventListener("click",(evento) =>{
+    evento.preventDefault();
+    if (frenteFC.value === "" || trasFC.value ==="") {
+        alert("nao porra");
+        return
+    }
+    const frentedoflash = frenteFC.value;
+    const trasdoflash = trasFC.value;
+    const FlashCard = document.createElement("button");
+    FlashCard.classList.add("FlashCard");
+    FlashCard.innerText = frentedoflash;
+    containerFC.appendChild(FlashCard);
+
+    FlashCard.addEventListener("click",(evento) =>{
+        evento.preventDefault();
+        if (FlashCard.innerText === frentedoflash){
+            FlashCard.innerText = trasdoflash;
+            containerFC.appendChild(FlashCard);
+        } else{
+            FlashCard.innerText = frentedoflash;
+            containerFC.appendChild(FlashCard);
+        }
+        
+    })
+
+})
